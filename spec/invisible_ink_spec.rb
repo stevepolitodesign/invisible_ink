14e79ee73a65931e00a7401a963e5fbf3342b9ca
Steve Polito <stevepolito@hey.com>
2023-03-15 07:35:15 -0400

    Create empty executable. Add test helper.
    This is be the basis for the gem, and as such I wanted to create a
    simple test to ensure the file exists and be executed without error.
    
    Additionally, I created a simple helper that wraps a system call to the
    executable.

--- spec/invisible_ink_spec.rb
+++ spec/invisible_ink_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe InvisibleInk do
   it "has a version number" do
     expect(InvisibleInk::VERSION).not_to be nil
   end
+
+  describe "invisible_ink executable" do
+    it "exits with a 0 status code" do
+      system_output = invoke_executable
+
+      expect(system_output).to be_truthy
+      expect($?).to be_success
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,12 @@
 
 require "invisible_ink"
 
+module InvisibleInkHelpers
+  def invoke_executable
+    system("exe/invisible_ink")
+  end
+end
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
@@ -12,4 +18,6 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.include InvisibleInkHelpers
 end
