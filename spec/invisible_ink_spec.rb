2f927ab37e9246bf747d703feb7500280edae4f1
Steve Polito <stevepolito@hey.com>
2023-03-15 07:54:41 -0400

    Add write command
    Introduce `write` command without any implementation.
    
    ```sh
    invisible_ink write file.txt
    ```

--- spec/invisible_ink_spec.rb
+++ spec/invisible_ink_spec.rb
@@ -6,11 +6,29 @@ RSpec.describe InvisibleInk do
   end
 
   describe "invisible_ink executable" do
-    it "exits with a 0 status code" do
-      system_output = invoke_executable
+    it "requires a command" do
+      system_output = invoke_executable(nil)
 
-      expect(system_output).to be_truthy
-      expect($?).to be_success
+      expect(system_output).to be_falsey
+      expect($?).to_not be_success
+    end
+
+    describe "write command" do
+      it "exits with a 0 status code" do
+        system_output = invoke_executable("write", file: "file.txt")
+
+        expect(system_output).to be_truthy
+        expect($?).to be_success
+      end
+
+      context "when a file is not passed" do
+        it "exits with a 1 status code" do
+          system_output = invoke_executable("write")
+
+          expect(system_output).to be_falsey
+          expect($?).to_not be_success
+        end
+      end
     end
   end
 end
